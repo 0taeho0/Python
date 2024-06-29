0bc6739f04f00d383dde5107196f30982e17933e
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-06-29 16:26:43 +0900

    Create list.py

---
+++ Sand_Game/list.py
@@ -0,0 +1,5 @@
+a_list, b_list, c_list, d_list, e_list, f_list, g_list = [], [], [], [], [], [], [] #병
+
+color_list = [red, blue, yellow, green, pink] #색을 모아둔 리스트
+
+bottle_list = [a_list, b_list, c_list, d_list, e_list, f_list, g_list] #병을 모아둔 리스트
